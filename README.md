68e660c425f18345085bb4b9fe3a0fb709799f60
Luca Zorzi <luca@lucazorzi.net>
2022-05-08 11:30:12 +0200

    Some nicer formatting

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # worker-feed-from bucket
+
 This Cloudflare Worker generates a podcast RSS feed based on the contents of a S3-compatible bucket.
 It is based on [worker-signed-s3-template](https://github.com/obezuk/worker-signed-s3-template)
 
 ## Configuration
+
 You have to create your own `wrangler.toml`, which you can base on [`wrangler.toml.example`](https://github.com/LucaTNT/worker-feed-from-bucket/blob/main/wrangler.toml.example).
 
 Configuration is done through environment variables:
 
-* `AWS_DEFAULT_REGION`: the default S3 region. It is blank with some providers (e.g. Backblaze).
-* `AWS_S3_BUCKET`: The virtualhost-style S3 endpoint for your bucket. (e.g. `mybucket.s3.eu-west-003.backblazeb2.com` or `mybucket.s3.eu-south-1.amazonaws.com`).
-* `BUCKET_PUBLIC_URL`: The base URL of your bucket. Usually it is `https://${AWS_S3_BUCKET}`, but sometimes it is useful to change it (for example if you have Cloudflare in front of your S3 storage).
-* `SUBFOLDER`: Optional subfolder of the bucket to search files in.
-* `PODCAST_TITLE`: The title that appears on the RSS feed.
-* `PODCAST_DESCRIPTION`: The description that appears on the RSS feed.
-* `PODCAST_IMAGE`: The URL of an image to be used as the podcast album art.
-* `PODCAST_IMAGE_WIDTH`: The width of `PODCAST_IMAGE`
-* `PODCAST_IMAGE_HEIGHT`: The height of `PODCAST_IMAGE`
+- `AWS_DEFAULT_REGION`: the default S3 region. It is blank with some providers (e.g. Backblaze).
+- `AWS_S3_BUCKET`: The virtualhost-style S3 endpoint for your bucket. (e.g. `mybucket.s3.eu-west-003.backblazeb2.com` or `mybucket.s3.eu-south-1.amazonaws.com`).
+- `BUCKET_PUBLIC_URL`: The base URL of your bucket. Usually it is `https://${AWS_S3_BUCKET}`, but sometimes it is useful to change it (for example if you have Cloudflare in front of your S3 storage).
+- `SUBFOLDER`: Optional subfolder of the bucket to search files in.
+- `PODCAST_TITLE`: The title that appears on the RSS feed.
+- `PODCAST_DESCRIPTION`: The description that appears on the RSS feed.
+- `PODCAST_IMAGE`: The URL of an image to be used as the podcast album art.
+- `PODCAST_IMAGE_WIDTH`: The width of `PODCAST_IMAGE`
+- `PODCAST_IMAGE_HEIGHT`: The height of `PODCAST_IMAGE`
 
 #### Wrangler
+
 Generation using [wrangler](https://github.com/cloudflare/wrangler) (`wrangler generate projectname https://github.com/LucaTNT/worker-feed-from-bucket`) is currently not supported due to this repo using the "main" branch as opposed to the "master" branch that wrangler is expecting.
 
 After you cloned the repo, set the required secrets containing your S3 API Keys:
--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-import { AwsClient } from 'aws4fetch'
+import { AwsClient } from "aws4fetch";
 
-const xml2js = require('xml2js');
+const xml2js = require("xml2js");
 const parser = new xml2js.Parser({ attrkey: "ATTR" });
 
 const aws = new AwsClient({
-    "accessKeyId": (globalThis.AWS_ACCESS_KEY_ID || ''), // This is required when creating a new environment that doesn't yet have secrets in place
-    "secretAccessKey": (globalThis.AWS_SECRET_ACCESS_KEY || ''),
-    "region": AWS_DEFAULT_REGION,
+  accessKeyId: globalThis.AWS_ACCESS_KEY_ID || "", // This is required when creating a new environment that doesn't yet have secrets in place
+  secretAccessKey: globalThis.AWS_SECRET_ACCESS_KEY || "",
+  region: AWS_DEFAULT_REGION,
 });
 
-addEventListener('fetch', function(event) {
-    event.respondWith(handleRequest(event.request));
+addEventListener("fetch", function(event) {
+  event.respondWith(handleRequest(event.request));
 });
 
 const PUBLIC_URL = globalThis.BUCKET_PUBLIC_URL || `https://${AWS_S3_BUCKET}/`;
-var BUCKET_SUBFOLDER = globalThis.SUBFOLDER || '';
+var BUCKET_SUBFOLDER = globalThis.SUBFOLDER || "";
 
-BUCKET_SUBFOLDER = (!BUCKET_SUBFOLDER.endsWith('/') && BUCKET_SUBFOLDER != '') ? `${BUCKET_SUBFOLDER}/` : BUCKET_SUBFOLDER;
+BUCKET_SUBFOLDER =
+  !BUCKET_SUBFOLDER.endsWith("/") && BUCKET_SUBFOLDER != ""
+    ? `${BUCKET_SUBFOLDER}/`
+    : BUCKET_SUBFOLDER;
 
 function createURL(object_key) {
-    if (BUCKET_SUBFOLDER != ""){
-        object_key = object_key.replace(BUCKET_SUBFOLDER, '');
-    }
+  if (BUCKET_SUBFOLDER != "") {
+    object_key = object_key.replace(BUCKET_SUBFOLDER, "");
+  }
 
-    return `${PUBLIC_URL}${BUCKET_SUBFOLDER}${encodeURIComponent(object_key)}`;
+  return `${PUBLIC_URL}${BUCKET_SUBFOLDER}${encodeURIComponent(object_key)}`;
 }
 
 function createFeed(files) {
-    var rss = `<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:content="http://purl.org/rss/1.0/modules/content/" version="2.0">
+  var rss = `<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:content="http://purl.org/rss/1.0/modules/content/" version="2.0">
     <channel>
         <title>${PODCAST_TITLE}</title>
         <description>${PODCAST_DESCRIPTION}</description>
@@ -39,53 +42,56 @@ function createFeed(files) {
         </image>
         <itunes:image href="${PODCAST_IMAGE}" />\n`;
 
-    files.forEach(file => {
-        rss += '        <item>\n';
-        rss += `            <title>${file.title}</title>\n`
-        rss += `            <link>${file.url}</link>\n`
-        rss += `            <pubDate>${file.pubDate}</pubDate>\n`
-        rss += `            <guid isPermaLink="false">${file.url}</guid>\n`
-        rss += `            <enclosure url="${file.url}" length="${file.size}" type="audio/mpeg" />\n`
-        rss += '        </item>\n';
-    });
+  files.forEach(file => {
+    rss += "        <item>\n";
+    rss += `            <title>${file.title}</title>\n`;
+    rss += `            <link>${file.url}</link>\n`;
+    rss += `            <pubDate>${file.pubDate}</pubDate>\n`;
+    rss += `            <guid isPermaLink="false">${file.url}</guid>\n`;
+    rss += `            <enclosure url="${file.url}" length="${file.size}" type="audio/mpeg" />\n`;
+    rss += "        </item>\n";
+  });
 
-    return rss + '    </channel>\n</rss>\n';
+  return rss + "    </channel>\n</rss>\n";
 }
 
 async function handleRequest(request) {
-    var url = new URL(`https://${AWS_S3_BUCKET}/`);
-    url.search = `list-type=2&prefix=${BUCKET_SUBFOLDER}`
-    var signedRequest = await aws.sign(url);
+  var url = new URL(`https://${AWS_S3_BUCKET}/`);
+  url.search = `list-type=2&prefix=${BUCKET_SUBFOLDER}`;
+  var signedRequest = await aws.sign(url);
 
-    var request = await fetch(signedRequest, { "cf": { "cacheEverything": true } });
+  var request = await fetch(signedRequest, { cf: { cacheEverything: true } });
 
-    const text = await request.text()
-    var files = [];
+  const text = await request.text();
+  var files = [];
 
-    console.log(text);
+  console.log(text);
 
-    const result = await new Promise((resolve, reject) => parser.parseString(text, (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
-    }));
+  const result = await new Promise((resolve, reject) =>
+    parser.parseString(text, (err, result) => {
+      if (err) reject(err);
+      else resolve(result);
+    }),
+  );
 
-    if ('Contents' in result['ListBucketResult']) {
-        const contents = result['ListBucketResult']['Contents'];
+  if ("Contents" in result["ListBucketResult"]) {
+    const contents = result["ListBucketResult"]["Contents"];
 
-        contents.forEach(element => {
-            files.push({
-                "title": `<![CDATA[${element['Key'][0].replace(BUCKET_SUBFOLDER, '')}]]>`,
-                "pubDate": element['LastModified'][0],
-                "size": element['Size'][0],
-                "url": createURL(element['Key'][0])
-            })
-        })
-    }
+    contents.forEach(element => {
+      files.push({
+        title: `<![CDATA[${element["Key"][0].replace(BUCKET_SUBFOLDER, "")}]]>`,
+        pubDate: element["LastModified"][0],
+        size: element["Size"][0],
+        url: createURL(element["Key"][0]),
+      });
+    });
+  }
 
-    return new Response(createFeed(files), {
-        headers: {
-          "content-type": "application/rss+xml;charset=UTF-8",
-      }})
+  return new Response(createFeed(files), {
+    headers: {
+      "content-type": "application/rss+xml;charset=UTF-8",
+    },
+  });
 
-    // return await fetch(signedRequest, { "cf": { "cacheEverything": true } });
+  // return await fetch(signedRequest, { "cf": { "cacheEverything": true } });
 }
